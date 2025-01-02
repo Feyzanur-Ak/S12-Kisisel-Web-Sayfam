ddb042664e7efe824c63ed778d582a4babfcb93b
Feyzanur-Ak <senturkfeyzanur96@gmail.com>
2025-01-02 10:29:26 +0300

    Store.jsx oluşturuldu

---
+++ web-sayfam/src/store/store.jsx
@@ -0,0 +1,10 @@
+import { createStore, combineReducers } from "redux";
+import userReducer from "./reducers/userReducer";
+
+const rootReducer = combineReducers({
+  user: userReducer, // Kullanıcı bilgilerini yöneten reducer
+});
+
+const store = createStore(rootReducer);
+
+export default store;
