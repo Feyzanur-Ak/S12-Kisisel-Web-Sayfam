894399013738b6a6da35a56aac367cf79d57db70
Feyzanur-Ak <senturkfeyzanur96@gmail.com>
2025-01-02 10:27:52 +0300

    Paket güncellemeleri

--- web-sayfam/src/main.jsx
+++ web-sayfam/src/main.jsx
@@ -2,12 +2,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { GlobalProvider } from "./context/GlobalContext";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux"; // Redux Provider
+import store from "./store/store"; // Redux store
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <GlobalProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </GlobalProvider>
+  <Provider store={store}> {/* Redux store'u sağla */}
+    <GlobalProvider> {/* Global context */}
+      <BrowserRouter> {/* Routing */}
+        <App />
+      </BrowserRouter>
+    </GlobalProvider>
+  </Provider>
 );
