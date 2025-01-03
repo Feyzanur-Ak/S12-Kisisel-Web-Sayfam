d5d8c0fc2d3b92ec9cbc1d42b24e8e9b72908c0e
Feyzanur-Ak <senturkfeyzanur96@gmail.com>
2025-01-03 13:05:11 +0300

    useLocalStorage düzenlendi

--- web-sayfam/src/hooks/useLocalStorage.jsx
+++ web-sayfam/src/hooks/useLocalStorage.jsx
@@ -2,38 +2,15 @@ import { useState } from "react";
 
 export const useLocalStorage = (key, initialValue) => {
   const [data, setData] = useState(() => {
-    try {
-      const storedData = localStorage.getItem(key);
-
-   
-
-      if (!storedData || storedData === "undefined") {
-        localStorage.setItem(key, JSON.stringify(initialValue));
-        return initialValue;
-      }
-
-     
-      return JSON.parse(storedData);
-    } catch (error) {
-      console.error(`Error accessing localStorage key "${key}":`, error);
-      return initialValue;
-    }
+    const storedData = JSON.parse(localStorage.getItem(key));
+    initialValue = storedData ? storedData : initialValue;
+    localStorage.setItem(key, JSON.stringify(initialValue));
+    return initialValue;
   });
 
   const updateStorage = (newValue) => {
-    if (newValue === undefined) {
-      console.error(`Error: Trying to set "${key}" with an undefined value.`);
-      return;
-      
-    }
-    console.log(newValue);
-
-    try {
-      localStorage.setItem(key, JSON.stringify(newValue));
-      setData(newValue);
-    } catch (error) {
-      console.error(`Error updating localStorage key "${key}":`, error);
-    }
+    localStorage.setItem(key, JSON.stringify(newValue));
+    setData(newValue);
   };
 
   return [data, updateStorage];
